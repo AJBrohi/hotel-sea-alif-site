e3179cf4cd920712c15c83895186d98b511793b4
Al-Junaed Islam <brohi96@gmail.com>
2021-02-22 13:45:46 +0600

    ekdom new

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,58 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     //owl.carosel
-    
-    
+
+
     $('.slider_section').owlCarousel({
-        loop:true,
-        margin:5,
-        responsiveClass:true,
-        autoplay:3000,
-        responsive:{
-            0:{
-                items:3,
-                nav:true
+        loop: true,
+        margin: 5,
+        responsiveClass: true,
+        autoplay: 3000,
+        responsive: {
+            0: {
+                items: 3,
+                nav: true
             },
-            600:{
-                items:3,
-                nav:false
+            600: {
+                items: 3,
+                nav: false
             },
-            1000:{
-                items:3,
-                nav:true,
-                loop:true
+            1000: {
+                items: 3,
+                nav: true,
+                loop: true
             }
         }
     })
-    
-    
-    
-    
-    
-        });
+});
+
+// This function will show the image in the lightbox
+var zoomImg = function () {
+    // Create evil image clone
+    var clone = this.cloneNode();
+    clone.classList.remove("zoomD");
+
+    // Put evil clone into lightbox
+    var lb = document.getElementById("lb-img");
+    lb.innerHTML = "";
+    lb.appendChild(clone);
+
+    // Show lightbox
+    lb = document.getElementById("lb-back");
+    lb.classList.add("show");
+};
+
+window.addEventListener("load", function () {
+    // Attach on click events to all .zoomD images
+    var images = document.getElementsByClassName("zoomPic");
+    if (images.length > 0) {
+        for (var img of images) {
+            img.addEventListener("click", zoomImg);
+        }
+    }
+
+    // Click event to hide the lightbox
+    document.getElementById("lb-back").addEventListener("click", function () {
+        this.classList.remove("show");
+    })
+});
